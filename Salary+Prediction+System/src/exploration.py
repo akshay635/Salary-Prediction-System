6ef47ba91e653fe9e0b952337dd6bdd3cf9cd9b9
Akshay Atanure <akshayatanure11@gmail.com>
2026-01-31 19:00:33 +0530

    Update exploration.py

--- Salary+Prediction+System/src/exploration.py
+++ Salary+Prediction+System/src/exploration.py
@@ -9,23 +9,12 @@ def explore():
     
     df, salary_df, country_df, edlevel_df = load_data()
     
-    f1, f2, f3, f4, f5 = plot_visualization(country_df, salary_df, edlevel_df)
-
-    col1, col2 = st.columns(2)
-    
-    return f1
-    
-    with col1:
-        return f2
-
-    with col2:
-        return f3
-
-    return f4, f5
+    plot_visualization(country_df, salary_df, edlevel_df)
         
     
     
 
     
+
 
 
