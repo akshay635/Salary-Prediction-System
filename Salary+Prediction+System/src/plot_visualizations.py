6ba032d6a588e2cef40f2541fce6d25644d96f13
Akshay Atanure <akshayatanure11@gmail.com>
2026-01-31 13:55:04 +0530

    Update plot_visualizations.py

--- Salary+Prediction+System/src/plot_visualizations.py
+++ Salary+Prediction+System/src/plot_visualizations.py
@@ -4,9 +4,7 @@ import plotly.express as px
 import streamlit as st
 
 def plot_visualization(df_country, salary_df, edlvl_df):
-    threshold = 500
-    country_counts = df_country['Country'].value_counts()
-    df_country['Country_grouped'] = df_country['Country'].apply(lambda x: 'Others' if country_counts[x] < threshold else x)
+    df_country['Country_grouped'] = df_country['Country'].apply(lambda x: 'Others' if df_country['count'] < threshold else x)
     
     # pie chart
     fig1 = px.pie(data_frame=df_country, names='Country_grouped', values='count', hover_data='count', 
@@ -44,6 +42,7 @@ def plot_visualization(df_country, salary_df, edlvl_df):
     fig5 = st.plotly_chart(fig5, use_container_width=True) # storing the pie chart
     
     return fig1, fig2, fig3, fig4, fig5
+
 
 
 
