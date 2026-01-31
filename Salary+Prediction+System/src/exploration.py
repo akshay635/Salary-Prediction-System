286a5ffef415f19ad277cd98432bc98515bf7d8a
Akshay Atanure <akshayatanure11@gmail.com>
2026-01-31 18:58:28 +0530

    Update exploration.py

--- Salary+Prediction+System/src/exploration.py
+++ Salary+Prediction+System/src/exploration.py
@@ -3,14 +3,29 @@
 
 from src.load_data import load_data
 from src.plot_visualizations import plot_visualization
+import streamlit as st
 
 def explore():
     
     df, salary_df, country_df, edlevel_df = load_data()
     
-    plot_visualization(country_df, salary_df, edlevel_df)
+    f1, f2, f3, f4, f5 = plot_visualization(country_df, salary_df, edlevel_df)
+
+    col1, col2 = st.columns(2)
+    
+    return f1
+    
+    with col1:
+        return f2
+
+    with col2:
+        return f3
+
+    return f4, f5
+        
     
     
 
     
+
 
