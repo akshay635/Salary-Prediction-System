0c4ffa8a3e04c443290576bd61c4e12dfc62d65b
Akshay Atanure <akshayatanure11@gmail.com>
2026-01-31 12:29:12 +0530

    Update plot_visualizations.py

--- Salary+Prediction+System/src/plot_visualizations.py
+++ Salary+Prediction+System/src/plot_visualizations.py
@@ -3,7 +3,7 @@
 import plotly.express as px
 import streamlit as st
 
-def plot_visualization(df_country, salary_df, employment_df):
+def plot_visualization(df_country, salary_df, edlvl_df):
     
     #pie chart
     fig1 = px.pie(data_frame=df_country, names='Country', values='count', hover_data='count',
@@ -13,7 +13,7 @@ def plot_visualization(df_country, salary_df, employment_df):
 
     # bar chart for top 10 countries
     fig2 = px.bar(data_frame=salary_df.head(10), x='Salary', y='Country', color = 'Salary',
-                  title='Top 10 Countries with the mean salary for software developers (in $) in 2025',
+                  title='Top 10 Countries with highly earning software developers (in $) in 2025',
                   color_continuous_scale="Viridis")
     fig2.update_layout(yaxis=dict(autorange="reversed"))
 
@@ -22,7 +22,7 @@ def plot_visualization(df_country, salary_df, employment_df):
     # bar chart for bpttom 10 countries
     fig3 = px.bar(data_frame=salary_df.tail(10), x='Salary', y='Country', color='Salary', 
                   color_continuous_scale="Viridis",
-                  title='Bottom 10 Countries with the mean salary for software developers (in $) in 2025')
+                  title='Bottom 10 Countries with the lower mean salary for software developers (in $) in 2025')
 
     fig3 = st.plotly_chart(fig3, use_container_width=True) # storing the bar chart
 
@@ -32,13 +32,16 @@ def plot_visualization(df_country, salary_df, employment_df):
 
     fig4 = st.plotly_chart(fig4, use_container_width=True) # storing the choropleth plot
 
-    #pie chart
-    fig5 = px.pie(data_frame=employment_df, names='Employment', values='count', hover_data='count',
-                  title='Employment rate in 2025')
+    # bar chart for mean salary as per the education levels
+    fig5 = px.bar(data_frame=edlvl_df, x='Salary', y='EdLevel', color = 'Salary',
+                  title='The mean salary for software developers (in $) in 2025 as per the education level',
+                  color_continuous_scale="Viridis")
+    fig5.update_layout(yaxis=dict(autorange="reversed"))
 
     fig5 = st.plotly_chart(fig5, use_container_width=True) # storing the pie chart
     
     return fig1, fig2, fig3, fig4, fig5
+
 
 
 
